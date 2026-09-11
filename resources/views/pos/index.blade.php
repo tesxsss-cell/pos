5ec80e7c4700a773be2aad6f1908ae8cbadd1ff6
ijlal-code <ijlalcode@gmail.com>
2026-09-11 18:50:15 +0800

    ci/cd

--- resources/views/pos/index.blade.php
+++ resources/views/pos/index.blade.php
@@ -24,10 +24,11 @@
                 </div>
 
 
+
+
+
+
                 
-
-
-
                 
                 <div class="mt-3 flex flex-wrap gap-2">
                     <input id="cari" type="text" autocomplete="off"
