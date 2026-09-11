a7392ae5ba85b8280c313d5a51f180b492b82a09
ijlal-code <ijlalcode@gmail.com>
2026-09-11 14:46:08 +0800

    ci/cd

--- resources/views/pos/index.blade.php
+++ resources/views/pos/index.blade.php
@@ -22,8 +22,10 @@
                         </span>
                     @endif
                 </div>
-                
 
+
+
+                
                 <div class="mt-3 flex flex-wrap gap-2">
                     <input id="cari" type="text" autocomplete="off"
                            placeholder="Nama barang, SKU, atau tembak barcode ke sini"
